5a94dd013db1006641eb0193dffc51db0d8dec13
Nicolas Silva (Rato) <124286615+MouseRat9@users.noreply.github.com>
2025-03-31 18:25:22 -0300

    Update sqlite.js

--- src/database/sqlite.js
+++ src/database/sqlite.js
@@ -1,8 +1,10 @@
 import sqlite3 from "sqlite3";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const SQLite = sqlite3.verbose();
 
-// Pega o caminho do banco de dados da variável de ambiente DB_PATH ou usa o caminho local padrão
 const dbPath = process.env.DB_PATH || "./src/database/banco.db";
 
 // Função para executar consultas no banco
@@ -15,9 +17,13 @@ function query(command, params, method = 'all') {
     });
 }
 
-// Conectar ao banco de dados com o caminho configurado
 const db = new SQLite.Database(dbPath, SQLite.OPEN_READWRITE, (err) => {
-    if (err) return console.log("Erro ao conectar com banco: " + err.message);
+    if (err) {
+        console.error("Erro ao conectar com banco: " + err.message);
+        return;
+    }
+    console.log(`Conectado ao banco de dados: ${dbPath}`);
 });
 
 export { db, query };
+
